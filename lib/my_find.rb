0d304f29fe4ad3e85ed3da1d007e504545309948
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-20 01:29:51 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,7 @@ def my_find(collection)
   counter = 0
   while counter < collection.length
     if yield(collection[counter])
-      return colleciton[counter]
+      return collection[counter]
     end
     counter += 1
   end
